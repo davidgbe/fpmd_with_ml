e2de60a39b50279fc15a394935f09477f1558185
David Bell <davidgbe@usc.edu>
2017-03-10 08:20:56 -0800

    with set params

--- lib/gaussian_process/model.py
+++ lib/gaussian_process/model.py
@@ -9,7 +9,7 @@ from functools import partial
 class GaussianProcess:
     def __init__(self, covariance_func=None):
         self.covariance_func = default_covariance_func if covariance_func is None else covariance_func
-        self.hyperparams = {'theta_amp': 1.0, 'theta_length': 1.0 }
+        self.hyperparams = {'theta_length': -102377628.35222179, 'theta_amp': 17493619.372016508}
         self.covariance_func = partial(self.covariance_func, hyperparams=self.hyperparams)
 
     def single_predict(self, target_x, training_cov_inv, Y_t, X):
