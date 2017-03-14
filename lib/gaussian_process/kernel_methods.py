b55c4853c65331ecd8aba1502f8424b5d3462a17
David Bell <davidgbe@usc.edu>
2017-03-13 21:19:15 -0700

    print chunk size

--- lib/gaussian_process/kernel_methods.py
+++ lib/gaussian_process/kernel_methods.py
@@ -57,6 +57,8 @@ def cartesian_operation(X_1, X_2=None, function=None, cores=None, cached_pool=No
 
     chunk_size_1 = int(sqrt(rows_1 * rows_2 / cores))
     chunk_size_2 = chunk_size_1
+    print('chunk size:')
+    print(chunk_size_1)
 
     iter_size_1 = chunk_size_1 * cols
     iter_size_2 = chunk_size_2 * cols
