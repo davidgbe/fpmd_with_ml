37f40e00a9be640bff9cdefc6989e397d0b80f6d
David Bell <davidgbe@usc.edu>
2017-04-01 19:58:08 -0700

    remove prints

--- lib/internal_vector/utilities.py
+++ lib/internal_vector/utilities.py
@@ -13,8 +13,6 @@ def produce_internal_basis(atomic_config_mat, r_cut=1.0, p=1.0):
             if row != origin_row:
                 displacement_vec = (atomic_config_mat[row] - origin_vec).reshape(origin_vec.shape[1])
                 mag_displacement = norm(displacement_vec)
-                print(displacement_vec.shape)
-                print(mag_displacement)
                 unit = displacement_vec / mag_displacement
                 internal_basis_vec += (unit * exp(-1*(mag_displacement / r_cut)**p))
         new_basis.append(internal_basis_vec)
@@ -23,9 +21,5 @@ def produce_internal_basis(atomic_config_mat, r_cut=1.0, p=1.0):
 def produce_feature_matrix(basis_mat):
     mags = np.apply_along_axis(norm, 1, basis_mat)
     mags = mags.reshape(mags.shape[0], 1)
-    print(mags)
-    print(basis_mat)
     v_norm_trans = np.divide(basis_mat, mags).T
-    print(v_norm_trans)
     return basis_mat.dot(v_norm_trans)
-
