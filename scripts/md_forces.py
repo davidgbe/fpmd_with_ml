9fb6d5c130d885e817f01bf18242a5032d7da65f
David Bell <davidgbe@usc.edu>
2017-04-23 18:06:03 -0700

    let's try a lot of examples randomly sampled

--- scripts/md_forces.py
+++ scripts/md_forces.py
@@ -10,8 +10,8 @@ import gc
 class MDForcesPredictor:
     @staticmethod
     def predict(data_path):
-        start = 4000
-        end = 4500
+        start = 3000
+        end = 6000
 
         # write first number of first arrangement used to make internal rep data in file
         internal_reps = MDForcesPredictor.load_data(data_path + '/iv_reps_108_1_to_6_half.txt', start - 1000, end - 1000)
@@ -22,8 +22,8 @@ class MDForcesPredictor:
 
         to_sample = [feature_mats, internal_reps_normed, forces, forces_k_space]      
         # sample only necessary examples 
-        num_examples = 450
-        (feature_mats, internal_reps_normed, forces, forces_k_space) = utilities.sample_population(to_sample, size=(num_examples + 50))
+        num_examples = 950
+        (feature_mats, internal_reps_normed, forces, forces_k_space) = utilities.sample_population(to_sample, size=(num_examples + 100))
 
         # free up examples not being used
         del to_sample
